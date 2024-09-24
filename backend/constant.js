2eecefccd575ec8d948c31bb27ba036fa2445722
Areeb Ahmed <ahmedareebkhalil@gmail.com>
2024-09-24 15:55:04 +0530

    1/13 done

--- backend/constant.js
+++ backend/constant.js
@@ -1,6 +1,6 @@
 class PUBLIC_DATA {
     static port = process.env.PORT || 4000;
-    static mongo_uri = process.env.MONGO_URI || 'mongodb://localhost/inventry';
+    static mongo_uri = process.env.MONGO_URI || 'mongodb+srv://ahmedareebkhalil:vCQ5DnZaXh20Gw8C@cluster0.becth.mongodb.net/';
 }
 
 module.exports = {
--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,19 @@ const express = require("express")
 const app  =express()
 const cors = require("cors")
 const morgan = require("morgan")
+const ApiError = require("./utils/ApiError")
+const ErrorHandling = require("./middlewares/ErrorHandler")
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json({limit:'10mb'}))
 app.use(express.urlencoded({extended:false}))
 
+app.use("/api/v1",require("./routes"))
 
-module.exports  =app
+app.use("*",(req,res)=>{
+    throw new ApiError(404,"ghpage not found");
+    
+})
+
+app.use(ErrorHandling)
+module.exports =app
